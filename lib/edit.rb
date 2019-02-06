20675c3c9bd87b26d545cae672c30ea9d39833a0
Martin Pickersgill <martinpick@yahoo.com>
2019-02-06 19:23:36 +0000

    generates a predicted filename

--- lib/edit.rb
+++ lib/edit.rb
@@ -3,16 +3,20 @@ require "../rspec_book_etc/hash_mod/lib/hash_mod"
 class Edit
   attr_reader :split_command
   attr_reader :predicted_filename
+  attr_reader :extensionless_filename
 
   def initialize (options = {})
     @handover_hash = options[:hash]
     @handover_array = options[:array]
     @tag_list = options[:tag_list].to_s || "tag list not set"
+    @discard_before = @handover_hash['discard_before'].to_f || "discard_before not set"
+    @discard_after  = @handover_hash['discard_after'].to_f || "discard_after not set"
     @file_name = @handover_hash['file_name'] || @handover_hash[:file_name] || "name_of_file not set"
-    @discard_before = sprintf("%.02f", @handover_hash['discard_before'].to_f) || "discard_before not set"
-    @discard_after = sprintf("%.02f", @handover_hash['discard_after'].to_f) || "discard_after not set"
-    @split_command = 'mp3splt ' + @file_name + ' ' + @discard_before + ' ' + @discard_after.to_s 
-    @predicted_filename = @file_name + @discard_before
+    @extensionless_filename = @file_name.gsub(/\..*/, '')
+    @discard_before_mins = sprintf("%02d", @discard_before) + 'm_' + sprintf("%02d", (@discard_before % 1) * 100) + 's__' || "discard_before not set"
+    @discard_after_mins = sprintf("%02d", @discard_after) + 'm_' + sprintf("%02d", (@discard_after % 1) * 100) + 's'
+    @split_command = 'mp3splt ' + @file_name + ' ' + sprintf("%.02f", @discard_before) + ' ' + sprintf("%.02f", @discard_after) 
+    @predicted_filename = @extensionless_filename + '_' + @discard_before_mins + @discard_after_mins + '.mp3'
   end
   def tag_command
     set_tags = String.new
@@ -23,7 +27,7 @@ class Edit
         end
       end
     end
-    @tag_command = 'id3v2 "' + @file_name + '"' + set_tags
+    @tag_command = 'id3v2 "' + @predicted_filename + '"' + set_tags
   end
 end
 
--- spec/edit_spec.rb
+++ spec/edit_spec.rb
@@ -3,15 +3,24 @@ require './lib/edit.rb'
 require '../rspec_book_etc/hash_mod/lib/hash_mod.rb'
 
 describe 'Edit' do
-  context 'generate predicted_filename'
+  context 'generate predicted_filename' do
     before :each do
       @tag_list = "genre,year,artist" 
-      @hash_version_of_data = {'file_name' => "180919_0688.MP3" 'discard_before'=> "0.53", 'discard_after' => "2", 'artist' => "Martin Pickersgill", 'album' => "Alberti", 'song' => "BWV847", 'genre' => "32", 'TYER' => "2018", 'TIT2' => "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"
-      @array_version_of_data = [["file_name", "discard_before", "discard_after", "artist", "album", "song", "genre", "TYER", "TIT2"]
-      @array_version_of_data = {'file_name' => "180919_0688.MP3" 'discard_before'=> "0.53", 'discard_after' => "2", 'artist' => "Martin Pickersgill", 'album' => "Alberti", 'song' => "BWV847", 'genre' => "32", 'TYER' => "2018", 'TIT2' => "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"
+      @hash_version_of_data = {'file_name' => "180919_0688.MP3", 'discard_before' => "0.53", 'discard_after' => "2", 'artist' => "Martin Pickersgill", 'album' => "Alberti", 'song' => "BWV847", 'genre' => "32", 'TYER' => "2018", 'TIT2' => "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"}
+      @array_version_of_data = [["file_name", "discard_before", "discard_after", "artist", "album", "song", "genre", "TYER", "TIT2"],["180919_0688.MP3", "0.53", "2", "Martin Pickersgill", "Alberti", "BWV847", "32", "2018", "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"]]
+      options_predict = {:tag_list => @tag_list, :hash => @hash_version_of_data, :array => @array_version_of_data}
+      @edit_predict = Edit.new options_predict
       #mp3splt 180919_0688.MP3 0.53 2.00
-
     end
+    subject {@edit_predict}
+    it {should respond_to :extensionless_filename}
+    it 'can give the filename without an extension' do
+      expect(@edit_predict.extensionless_filename).to eq('180919_0688')
+    end
+    it 'predicts the filename resulting from the split' do
+      expect(@edit_predict.predicted_filename).to eq('180919_0688_00m_53s__02m_00s.mp3')
+    end
+  end
 
 
   context 'Basic Edit' do
@@ -20,23 +29,18 @@ describe 'Edit' do
       @hash_version_of_data = {'track' => "3", 'discard_before' => "27.5", 'discard_after' => "28.21", 'file_name' => "schmn39-48_luxford-evans_1901-12nov18.mp3", 'artist' => "C Luxford D Evans", 'album' => "C Luxford D Evans Schumann Liede 1901 Arts Nov 2018", 'song' => "Die Rose", 'genre' => "32", 'year' => "2018"}
       @array_version_of_data = [["track", "discard_before", "discard_after", "file_name", "artist", "album", "song", "genre", "year"], ["3", "27.5", "28.21", "schmn39-48_luxford-evans_1901-12nov18.mp3", "C Luxford D Evans", "C Luxford D Evans Schumann Liede 1901 Arts Nov 2018", "Die Rose", "32", "2018"]]
       options_hash = {:tag_list => @tag_list, :hash => @hash_version_of_data, :array => @array_version_of_data}
-      #options_hash = {:tag_list => "genre,year,artist", :hash => {:track => "3", :discard_before => "27.5", :discard_after => "28.21", :file_name => "schmn39-48_luxford-evans_1901-12nov18.mp3", :artist => "C Luxford D Evans", :album => "C Luxford D Evans Schumann Liede 1901 Arts Nov 2018", :song => "Die Rose", :genre => "32", :year => "2018"}, :array=>[["track", "discard_before", "discard_after", "file_name", "artist", "album", "song", "genre", "year"], ["3", "27.5", "28.21", "schmn39-48_luxford-evans_1901-12nov18.mp3", "C Luxford D Evans", "C Luxford D Evans Schumann Liede 1901 Arts Nov 2018", "Die Rose", "32", "2018"]]}
-      #puts "in edit_spec.rb, options_hash is: " + options_hash.inspect
-      #options_hash = {:hash_version => { :track =>"3", :discard_before => "27.5"}}
-      #@edit1 = Edit.new :file_name => '180908_0688.MP3', :discard_before => '02.18', :discard_after => '03.00'
-      #@edit1 = Edit.new [['file_name', 'discard_before', 'discard_after'],['180908_0688.MP3', '02.18', '03.00']]
       @edit1 = Edit.new options_hash
     end
     subject {@edit1}
     it {should respond_to :split_command}
     it {should respond_to :predicted_filename}
     it 'predicts the filename resulting from the split' do
-      expect(@edit1.predicted_filename).to eq('180919_0688_27m_50s__28m_21s')
+      expect(@edit1.predicted_filename).to eq('schmn39-48_luxford-evans_1901-12nov18_27m_50s__28m_21s.mp3')
     end
     it 'produces a command to split the file at the split point' do
       expect(@edit1.split_command).to eq('mp3splt schmn39-48_luxford-evans_1901-12nov18.mp3 27.50 28.21')
     end
-    xit 'produces a command to add tags to the file' do
+    it 'produces a command to add tags to the file' do
       expect(@edit1.tag_command).to eq('id3v2 schmn39-48_luxford-evans_1901-12nov18.mp3 --artist "C Luxford D Evans" --genre "32" --year "2018"')
     end
   end
@@ -44,7 +48,7 @@ describe 'Edit' do
     before :each do    
       options2_hash = {:tag_list => "genre,year,artist", :hash => {:track => "3", :discard_before => "", :discard_after => "", :file_name => "schmn39-48_luxford-evans_1901-12nov18.mp3", :artist => "C Luxford D Evans", :album => "C Luxford D Evans Schumann Liede 1901 Arts Nov 2018", :song => "Die Rose", :genre => "32", :year => "2018"}, :array=>[["track", "discard_before", "discard_after", "file_name", "artist", "album", "song", "genre", "year"], ["3", "27.5", "28.21", "schmn39-48_luxford-evans_1901-12nov18.mp3", "C Luxford D Evans", "C Luxford D Evans Schumann Liede 1901 Arts Nov 2018", "Die Rose", "32", "2018"]]}
       @edit_no_split =Edit.new options2_hash
-      end  
+    end  
       subject {@edit_no_split}
       it 'does not respond produce a split command unless discard_before and discard_after are present' do
         expect(@edit_no_split.split_command).to eq('')
