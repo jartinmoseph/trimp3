cbfd075d59987d52ec9081e4181afcaafa6e6e7d
Martin Pickersgill <martinpick@yahoo.com>
2019-02-19 16:56:50 +0000

    new example

--- spec/edit2_spec.rb
+++ spec/edit2_spec.rb
@@ -9,8 +9,17 @@ describe 'Edit' do
       @hash_version_of_data = {'file_name' => "180919_0688.MP3", 'discard_before' => "0.53", 'discard_after' => "2", 'artist' => "Martin Pickersgill", 'album' => "Alberti", 'song' => "BWV847", 'genre' => "32", 'TYER' => "2018", 'TIT2' => "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"}
       @array_version_of_data = [["file_name", "discard_before", "discard_after", "artist", "album", "song", "genre", "TYER", "TIT2"],["180919_0688.MP3", "0.53", "2", "Martin Pickersgill", "Alberti", "BWV847", "32", "2018", "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"]]
       options_ffmpeg = {:tag_list => @tag_list, :hash => @hash_version_of_data, :array => @array_version_of_data}
-      @edit_ffmpeg = Edit.new options_predict
-      #mp3splt 180919_0688.MP3 0.53 2.00
+      @edit_ffmpeg = Edit.new options_ffmpeg
+    end
+    subject {@edit_ffmpeg}
+    it {should respond_to :edit_by_ffmpeg}
+    it 'can return an ffmpeg command to split the file' do
+      expect(@edit_ffmpeg.edit_by_ffmpeg).to eq('ffmpeg -ss 53 -t 1.07 -i 180919_0688.MP3 -acodec copy bwv847_martin-pickersgill_19sept19')
+    end
+  end
+end
+
+
 =begin
 describe 'Calculator' do
   describe '#calculate' do
@@ -21,15 +30,5 @@ describe 'Calculator' do
     end
   end
 end
+ hh:mm:ss[.xxx] syntax is also supported in ffmpeg
 =end
-    end
-    subject {@edit_ffmpeg}
-    it {should respond_to :extensionless_filename}
-    it 'can give the filename without an extension' do
-      expect(@edit_predict.extensionless_filename).to eq('180919_0688')
-    end
-    it 'predicts the filename resulting from the split' do
-      expect(@edit_predict.predicted_filename).to eq('180919_0688_00m_53s__02m_00s.mp3')
-    end
-  end
-end
