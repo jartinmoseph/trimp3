a01ca4b0fb3e6f332fb0031ab1645e25b14a487b
Martin Pickersgill <martinpick@yahoo.com>
2019-02-25 16:54:29 +0000

    hours min seconds examples added

--- spec/edit2_spec.rb
+++ spec/edit2_spec.rb
@@ -6,8 +6,18 @@ describe 'Edit' do
   context 'split by ffmpeg' do
     before :each do
       @tag_list = "genre,year,artist" 
-      @hash_version_of_data = {'file_name' => "180919_0688.MP3", 'discard_before' => "0.53", 'discard_after' => "2", 'artist' => "Martin Pickersgill", 'album' => "Alberti", 'song' => "BWV847", 'genre' => "32", 'TYER' => "2018", 'TIT2' => "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"}
-      @array_version_of_data = [["file_name", "discard_before", "discard_after", "artist", "album", "song", "genre", "TYER", "TIT2"],["180919_0688.MP3", "0.53", "2", "Martin Pickersgill", "Alberti", "BWV847", "32", "2018", "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"]]
+      @hash_version_of_data = {'file_name' => "180919_0688.MP3", 'discard_before_hours' => "0", 'discard_before_mins' => "0", 'discard_before_seconds' => "53", 'discard_after_hours' => "0", 'discard_after_mins' => "2", 'discard_after_seconds' => "0", 'discard_before' => "0.53", 'discard_after' => "2", 'artist' => "Martin Pickersgill", 'album' => "Alberti", 'song' => "BWV847", 'genre' => "32", 'TYER' => "2018", 'TIT2' => "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"}
+      @array_version_of_data = [["file_name", 
+
+"discard_before_hours", 
+"discard_before_mins", 
+"discard_before_seconds", 
+"discard_before"
+"discard_after_hours", 
+"discard_after_mins", 
+"discard_after_seconds", 
+"discard_after", 
+"artist", "album", "song", "genre", "TYER", "TIT2"],["180919_0688.MP3", "0", "0", "53", "0.53", "0", "2", "0", "2", "Martin Pickersgill", "Alberti", "BWV847", "32", "2018", "Martin Pickersgill, J S Bach, Prelude in C Minor, Bk 1, Alberti, 19 Sept 2018"]]
       options_ffmpeg = {:tag_list => @tag_list, :hash => @hash_version_of_data, :array => @array_version_of_data}
       @edit_ffmpeg = Edit.new options_ffmpeg
     end
